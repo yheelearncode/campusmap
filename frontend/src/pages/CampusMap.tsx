9c376246fa10a2d1259f091d32b7f102857edc3a
bezud94 <bezud.77@gmail.com>
2025-11-28 15:42:08 +0900

    기본 UI 번역 텍스트 분리

--- frontend/src/pages/CampusMap.tsx
+++ frontend/src/pages/CampusMap.tsx
@@ -6,7 +6,8 @@ import Form from 'react-bootstrap/Form';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
-import { Nav } from "react-bootstrap";
+
+import {ui_translations} from './constants/translations'
 
 declare global {
   interface Window {
@@ -90,79 +91,6 @@ interface ScheduleSidebarProps {
   handleClose: () => void;
   events: EventDetail[];
   onEventClick: (event: EventDetail) => void;
-}
-
-// 기본 UI 언어
-const ui_translations = {
-  ko: {
-    main: {
-      title: "캠퍼스 이벤트 지도",
-      add_event: "이벤트 추가",
-      add_guide: "지도에서 이벤트 위치를 클릭하세요",
-      logout: "로그아웃",
-      logout_check: "로그아웃 하시겠습니까?",
-      cancel: "취소",
-    },
-    add: {
-      title: "이벤트 등록",
-      title_placeholder: "제목 *",
-      description_placeholder: "내용 *",
-      post: "등록",
-      cancel: "취소",
-      success: "등록 완료!",
-      fail: "등록 실패"
-    },
-    detail: {
-      likes: "추천",
-      close: "닫기",
-    }
-  },
-  en: {
-    main: {
-      title: "Campus Event Map",
-      add_event: "Add Event",
-      add_guide: "Click the event location on the map",
-      logout: "Logout",
-      logout_check: "Logout?",
-      cancel: "Cancel",
-    },
-    add: {
-      title: "Post Event",
-      title_placeholder: "Title *",
-      description_placeholder: "Description *",
-      post: "Post",
-      cancel: "Cancel",
-      success: "Post Done!",
-      fail: "Post Failed"
-    },
-    detail: {
-      likes: "Likes",
-      close: "Close",
-    }
-  },
-  mn: {
-    main: {
-      title: "Кампусын арга хэмжээний газрын зураг",
-      add_event: "Арга хэмжээ нэмэх",
-      add_guide: "Газрын зураг дээр арга хэмжээний байршлыг сонгоно уу.",
-      logout: "로그아웃(mn)",
-      logout_check: "로그아웃 하시겠습니까?(mn)",
-      cancel: "취소(mn)",
-    },
-    add: {
-      title: "이벤트 등록(mn)",
-      title_placeholder: "제목 *(mn)",
-      description_placeholder: "내용 *(mn)",
-      post: "등록(mn)",
-      cancel: "취소(mn)",
-      success: "등록 완료!(mn)",
-      fail: "등록 실패(mn)"
-    },
-    detail: {
-      likes: "추천(mn)",
-      close: "닫기(mn)",
-    }
-  }
 }
 
 export default function CampusMap() {
